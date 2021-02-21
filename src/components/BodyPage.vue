fcf5a5de4acb459cb0f23af7962a986cc2caf72c
SHk <shbilly00@gmail.com>
2021-02-21 15:57:40 +0900

    modify bodypage

--- src/components/BodyPage.vue
+++ src/components/BodyPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="main-body">
-        <h1><span style="width:1.5rem;display:inline-block">{{ msg }}</span> Welcome to DevOps!</h1>
+        <h1><span class="move-box">{{ msg }}</span> Welcome to DevOps! <span class="move-box">{{ msg }}</span></h1>
     </div>  
 </template>
 
@@ -34,5 +34,10 @@ export default {
         color:#fff;
         font-size:3rem;
     }
+    move-box{
+        width:1.5rem;
+        display:inline-block;
+    }
+   
     
 </style>
